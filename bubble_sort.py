613616717a725eb9aff2524b2d18536b2665c893
Kai-Uwe Hinderer <kai.hinderer@thera-trainer.com>
2021-05-20 15:21:25 +0200

    bubble-sort puzzle in python

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+# Python program for implementation of Bubble Sort:
+#  - sort an arbitrary array handed as arg list (max size 20)
+#  - print unsorted list
+#  - print sorted list
+#
+# Example:
+# python bubble_sort.py 1 2 5 12 3
+# -> prints
+# unsorted: 1 2 5 12 3
+# sorted:   1 2 3 5 12 
