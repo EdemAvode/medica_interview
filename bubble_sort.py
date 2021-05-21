9b982b3576533e9017ae1a4680f1c80579eb9c3e
EdemAvode <84533454+EdemAvode@users.noreply.github.com>
2021-05-21 09:39:25 +0200

    Update bubble_sort.py
    Thank you.

--- bubble_sort.py
+++ bubble_sort.py
@@ -11,3 +11,48 @@
 
 
 # Magic Simon was here . __.
+
+#################################################################################
+
+
+##############################################################
+# Forgive me if the code is not an expert python Code.
+# I am more confortable in C or C++. :)
+# Beginner Python Code to perform a Buble Sorting
+##############################################################
+# main.py
+import sys
+
+#The Bubble Man
+def myBubbleSorting(theIputArray): 
+    myArrLght = len(theIputArray)
+    #Moving through array elements
+    for aLoop in range(0, myArrLght -1):
+        for bLoop in range (0 , myArrLght-aLoop-1 ):
+            #Move in the array from 0 to (myArrLght-aLoop-1)
+            #Swap if value at bLoop > value at bLoop+1
+            if theIputArray[bLoop] > theIputArray[bLoop+1] :
+                #Save the greater value temporarly
+                temp = theIputArray[bLoop]
+                #Save bLoop+1 at bLoop
+                theIputArray[bLoop] = theIputArray[bLoop+1]
+                #save now temp value at bLoop +1
+                theIputArray[bLoop+1] = temp
+
+if __name__ == "__main__":
+    #Store the user Argument
+    theIputArray = [int(ArrayElement) for ArrayElement in sys.argv[1:]]
+
+    #Print the Bubble sorted array
+    print ( "\n The Input Array Elements are:")
+    for myArrLoop in range (len(theIputArray)):
+        print("%d" %theIputArray[myArrLoop]),
+
+    # Now let us Bubble Sorted them 
+    myBubbleSorting(theIputArray)    
+    #Print the Bubble sorted array
+    print ( "\nThe Buble Sorted Elements are:")
+    for myArrLoop in range (len(theIputArray)):
+        print("%d" %theIputArray[myArrLoop]),
+
+        
